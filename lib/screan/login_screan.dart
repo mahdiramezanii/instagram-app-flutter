5cffd9197e12bef8a1793b62baf10cb1b000b2ad
mahdiramazani <mahdiramazani1281@gmail.com>
2024-02-09 00:24:43 +0330

    complated

--- lib/screan/login_screan.dart
+++ lib/screan/login_screan.dart
@@ -66,19 +66,23 @@ class LoginScrean extends StatelessWidget {
                     ],
                   ),
                   TextField(
-                    style: TextStyle(fontSize: 40),
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                    // textAlign: TextAlign.right,
+                    
                     decoration: InputDecoration(
-                      
+                      labelText: " Email ",
+                      hintText: "ایمیل خود را وارد کنید",
+                      hintStyle: TextStyle(color: Colors.white, fontSize: 15),
+                      labelStyle: TextStyle(color: Colors.white, fontSize: 20),
                       enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.white,width: 3),
-
-                      ) ,
-                      focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide(color: Colors.amber)),
+                        borderSide: BorderSide(color: Colors.white, width: 3),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.amber)),
                     ),
                   ),
-                 
                 ],
               ),
             ),
