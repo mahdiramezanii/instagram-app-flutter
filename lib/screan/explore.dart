dad4115d1457e9b1e08d81ada14e36a449c60c48
mahdiramazani <mahdiramazani1281@gmail.com>
2024-02-26 13:24:42 +0330

    add stagger grid to explore

--- lib/screan/explore.dart
+++ lib/screan/explore.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class ExploreScrean extends StatelessWidget {
   @override
@@ -73,6 +74,27 @@ class ExploreScrean extends StatelessWidget {
                     ),
                   );
                 },
+              ),
+            ),
+            Expanded(
+              child: GridView.custom(
+                gridDelegate: SliverQuiltedGridDelegate(
+                  crossAxisCount: 4,
+                  mainAxisSpacing: 4,
+                  crossAxisSpacing: 4,
+                  repeatPattern: QuiltedGridRepeatPattern.inverted,
+                  pattern: [
+                    QuiltedGridTile(2, 2),
+                    QuiltedGridTile(1, 1),
+                    QuiltedGridTile(1, 1),
+                    QuiltedGridTile(1, 2),
+                  ],
+                ),
+                childrenDelegate: SliverChildBuilderDelegate((context, index) {
+                  return Container(
+                    color: Colors.red,
+                  );
+                }),
               ),
             )
           ],
