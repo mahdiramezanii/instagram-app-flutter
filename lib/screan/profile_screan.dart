e2e26a8e563adb3ed1e87f457614d0b37093718c
mahdiramazani <mahdiramazani1281@gmail.com>
2024-03-05 00:51:53 +0330

    fix bug -> use nested scorol

--- lib/screan/profile_screan.dart
+++ lib/screan/profile_screan.dart
@@ -6,58 +6,89 @@ class ProfileScrean extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xff1C1F2E),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: Column(
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    width: 70,
-                    height: 70,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                bottom: PreferredSize(
+                  preferredSize: Size.fromHeight(20),
+                  child: Container(
+                    height: 20,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      border: Border.all(width: 3, color: Colors.pink),
+                      color: Color(0xff1C1F2E),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
+                      ),
                     ),
-                    child: Image.asset("assets/images/profile.png"),
                   ),
-                  SizedBox(
-                    width: 15,
+                ),
+                expandedHeight: 170,
+                actions: [],
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Image.asset(
+                    "assets/images/p9.jpg",
+                    fit: BoxFit.cover,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        "مهدی رمضانی برنامه نویس موبایل",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold
-                        ),
-                      ),
-                      Text(
-                        "Mahdiramezani.dev",
-                        style: TextStyle(
-                            color: Color.fromRGBO(197, 197, 197, 1),
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                  Spacer(),
-                  Image(
-                    image: AssetImage("assets/images/edit.png"),
+                ),
+              )
+            ];
+          },
+          body: Container(
+            color: Color(0xff1C1F2E),
+            child: _getHeader(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getHeader() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 70,
+            height: 70,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              border: Border.all(width: 3, color: Colors.pink),
+            ),
+            child: Image.asset("assets/images/profile.png"),
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "مهدی رمضانی برنامه نویس موبایل",
+                style: TextStyle(
                     color: Colors.white,
-                  ),
-                ],
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Mahdiramezani.dev",
+                style: TextStyle(
+                    color: Color.fromRGBO(197, 197, 197, 1),
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold),
               )
             ],
           ),
-        ),
+          Spacer(),
+          Image(
+            image: AssetImage("assets/images/edit.png"),
+            color: Colors.white,
+          ),
+        ],
       ),
     );
   }
