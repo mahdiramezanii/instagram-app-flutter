fe87ce87a148bd8db78508ea041e269c94b5cadb
mahdiramazani <mahdiramazani1281@gmail.com>
2024-03-04 23:56:50 +0330

    add heafer row in profile screan

--- lib/screan/profile_screan.dart
+++ lib/screan/profile_screan.dart
@@ -5,10 +5,58 @@ class ProfileScrean extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff1C1F2E),
-      body: Center(
-        child: Text(
-          "Mahdi ramezani",
-          style: TextStyle(color: Colors.white),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 70,
+                    height: 70,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(width: 3, color: Colors.pink),
+                    ),
+                    child: Image.asset("assets/images/profile.png"),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "مهدی رمضانی برنامه نویس موبایل",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      Text(
+                        "Mahdiramezani.dev",
+                        style: TextStyle(
+                            color: Color.fromRGBO(197, 197, 197, 1),
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  Spacer(),
+                  Image(
+                    image: AssetImage("assets/images/edit.png"),
+                    color: Colors.white,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
