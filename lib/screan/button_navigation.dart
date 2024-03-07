a271e80f20b0b2620732f5a4b1c613c546e0e636
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-03-07 23:14:33 +0330

    complated bottomNavigator

--- lib/screan/button_navigation.dart
+++ lib/screan/button_navigation.dart
@@ -10,16 +10,27 @@ class _ButtonNavigationState extends State<ButtonNavigationScrean> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.grey,
+      bottomNavigationBar: Container(
+        height: 85,
+        decoration: BoxDecoration(
+          color: Color(0xff1C1F2E),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(15),
+            topRight: Radius.circular(15),
+          ),
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
           fixedColor: Colors.red[900],
           currentIndex: _selectedIndex!,
           selectedFontSize: 20,
+          unselectedItemColor: Colors.white,
           selectedLabelStyle:
               TextStyle(color: Colors.amber, fontWeight: FontWeight.normal),
           showSelectedLabels: false,
-          // showUnselectedLabels: false,
-          iconSize: 30,
+          showUnselectedLabels: false,
+          iconSize: 35,
           type: BottomNavigationBarType.fixed,
           onTap: (int index) {
             setState(() {
@@ -28,13 +39,47 @@ class _ButtonNavigationState extends State<ButtonNavigationScrean> {
           },
           items: [
             BottomNavigationBarItem(
-                icon: Icon(Icons.sports_football_outlined), label: "mahdi5"),
+              // icon: Image.asset("assets/images/b1.png"),
+              icon: Icon(Icons.home_outlined),
+              label: "mahdi5",
+            ),
             BottomNavigationBarItem(
-                icon: Icon(Icons.menu_book), label: "mahdi4"),
-            BottomNavigationBarItem(icon: Icon(Icons.refresh), label: "mahdi3"),
+              // icon: Image.asset("assets/images/b2.png"),
+              icon: Icon(Icons.search),
+              label: "mahdi4",
+            ),
             BottomNavigationBarItem(
-                icon: Icon(Icons.abc_sharp), label: "mahdi"),
-          ]),
+              icon: Icon(Icons.add_circle_outline),
+              label: "mahdi3",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_outline),
+              label: "mahdi",
+            ),
+            BottomNavigationBarItem(
+              icon: Container(
+                height: 26,
+                width: 26,
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1,
+                      color:
+                          _selectedIndex == 4 ? Colors.red : Colors.transparent,
+                    ),
+                    
+                    borderRadius: BorderRadius.circular(6)),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(6),
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: Image.asset("assets/images/mahdi.jpeg"),
+                    )),
+              ),
+              label: "mahdi",
+            ),
+          ],
+        ),
+      ),
       body: Center(
         child: Text("Data"),
       ),
