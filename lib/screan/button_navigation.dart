6cd3b65fa1156ab5c13a0c379d05e560e71e573c
mahdiramezani-dev <mahdiramazanii@yahoo.com>
2024-03-07 22:45:48 +0330

    add bottomNavigator

--- lib/screan/button_navigation.dart
+++ lib/screan/button_navigation.dart
@@ -6,9 +6,35 @@ class ButtonNavigationScrean extends StatefulWidget {
 }
 
 class _ButtonNavigationState extends State<ButtonNavigationScrean> {
+  int? _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.grey,
+          fixedColor: Colors.red[900],
+          currentIndex: _selectedIndex!,
+          selectedFontSize: 20,
+          selectedLabelStyle:
+              TextStyle(color: Colors.amber, fontWeight: FontWeight.normal),
+          showSelectedLabels: false,
+          // showUnselectedLabels: false,
+          iconSize: 30,
+          type: BottomNavigationBarType.fixed,
+          onTap: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.sports_football_outlined), label: "mahdi5"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.menu_book), label: "mahdi4"),
+            BottomNavigationBarItem(icon: Icon(Icons.refresh), label: "mahdi3"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.abc_sharp), label: "mahdi"),
+          ]),
       body: Center(
         child: Text("Data"),
       ),
